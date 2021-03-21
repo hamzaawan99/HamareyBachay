d8bd7eb99766705c66908a86db7adbe6ac20fb7f
Hamza Awan <mh89371@gmail.com>
2021-03-22 01:08:32 +0500

    Add project files.

---
+++ HamareyBachay/Data/Migrations/20210319175532_intialsetup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HamareyBachay.Data.Migrations
+{
+    public partial class intialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    gender = table.Column<string>(nullable: false),
+                    DOB = table.Column<DateTime>(nullable: false),
+                    age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ HamareyBachay/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HamareyBachay.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public char gender { get; set; }
+        public DateTime DOB { get; set; }
+        public int age { get; set; }
+
+        public Student()
+        {
+
+        }
+    }
+}
